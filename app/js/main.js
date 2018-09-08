1843c56fc08590cf83fe33c2d95b133603a98071
marchik88 <marchik88@rambler.ru>
2018-09-08 20:21:15 +0300

    html css

--- app/js/main.js
+++ app/js/main.js
@@ -187,10 +187,7 @@ const GRVE = GRVE || {};
       $("#c-info__range").ionRangeSlider({
         type: "single",
         grid: true,
-        step: 0.25,
-        min: 1,
-        max: 4,
-        values: [0, 0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75, 2, 2.25, 2.5, 2.75, 3, 3.25, 3.5, 3.75, 4],
+        values: [250, 350, 450],
         hide_min_max: true,
       });
     }
