6b80281a72cb6d4218684618f449821729a18d6a
Security2431 <Artem2431@gmail.com>
2018-09-11 15:31:27 +0300

    fixtures move to global var

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {render} from 'react-dom'
 import registerServiceWorker from './registerServiceWorker'
 import App from './components/App'
-import {data} from './fixtures'
 
-render(<App data = {data} />, document.getElementById('calc'))
+
+render(<App data = {window.data} />, document.getElementById('calc'))
 registerServiceWorker()
